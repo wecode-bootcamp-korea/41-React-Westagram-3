dcd8da9767f187ca134844935fd1630b852f4910
오현주 <hyunjoooh@ohyeonjuui-MacBookPro.local>
2022-12-21 11:10:51 +0900

    초기세팅

---
+++ src/Router.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main/Main";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Router } from "react-router-dom";
+import Router from "./Router";
+import "./styles/commom.scss";
+import "./styles/reset.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<Router />);
